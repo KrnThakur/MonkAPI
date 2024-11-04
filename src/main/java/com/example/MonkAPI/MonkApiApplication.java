256d0a6ccb7c89e5fb3c31b1b6d18e3ed992a5dd
KrnThakur <85554672+KrnThakur@users.noreply.github.com>
2024-11-04 15:57:08 +0530

    TestCommit For Jenkins HookTrigger

--- src/main/java/com/example/MonkAPI/MonkApiApplication.java
+++ src/main/java/com/example/MonkAPI/MonkApiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MonkApiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Application Started");
 		SpringApplication.run(MonkApiApplication.class, args);
 	}
 
